08288261dd16a6860773f731d7416f5cf3169502
dev-avology <devavology12@gmail.com>
2026-09-10 14:16:18 +0530

    first commit

--- assests/js/index.js
+++ assests/js/index.js
@@ -85,3 +85,20 @@ function typeEffect() {
 spans.forEach(span => span.textContent = "");
 
 typeEffect();
+
+
+
+
+///////////////header////////////
+
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
